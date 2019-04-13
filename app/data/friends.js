7a93e9832819a05cf3065f8cb4c171aaa25a8996
JKRyan <jed@jedkendall.com>
2019-04-13 12:41:27 -0500

    Initial files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friendsArray = [];
+var questionAmount = 10;
+
+var Friend = function(name, photo, scores) {
+    this.name = name;
+    this.photo = photo;
+    this.scores = scores;
+}
+
+function randomScoreArray(number) {
+    var arr = [];
+    for (var i = 0; i < number; i++) {
+        arr.push(Math.floor(Math.random()*11));
+    }
+    return arr;
+}
+
+var sampleFriend = new Friend("Tane", "stockimage.jpg", randomScoreArray(questionAmount));
+
+friendsArray.push(sampleFriend);
+console.log(friendsArray);
