31dcf574350ea4e11953f62e854b5a03cf6eb67c
unknown <jed@jedkendall.com>
2019-04-21 22:19:06 -0500

    Trying this one more time

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './app/public')));
 app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
@@ -13,3 +14,4 @@ require("./app/routing/htmlRoutes")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
